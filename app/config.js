6600ac9f064d5b3e30e001d983261a6390732484
Pavel Mishakov <pashamishakov@gmail.com>
2021-04-30 19:13:08 +0600

    switched momgod atlas to local

--- app/config.js
+++ app/config.js
@@ -4,7 +4,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
     mongoUrl: {
-        url: "mongodb+srv://QWE123:QWE123@cluster0.rrd3k.mongodb.net/",
+        url: "mongodb://localhost/",
+        // url: "mongodb+srv://QWE123:QWE123@cluster0.rrd3k.mongodb.net/",
         db: "as_telega2"
     },
     telegramToken: "1514560951:AAFtVl-OZIRVrZNGmcsU6zgsmK8oKd1Qcfs",
--- app/config.ts
+++ app/config.ts
@@ -2,7 +2,8 @@
 
 export const config = {
     mongoUrl: {
-        url: "mongodb+srv://QWE123:QWE123@cluster0.rrd3k.mongodb.net/",
+        url: "mongodb://localhost/",
+        // url: "mongodb+srv://QWE123:QWE123@cluster0.rrd3k.mongodb.net/",
         db: `as_telega2`
     },
     telegramToken: "1514560951:AAFtVl-OZIRVrZNGmcsU6zgsmK8oKd1Qcfs", // main
