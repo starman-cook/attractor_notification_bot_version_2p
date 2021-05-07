9f9f288cb682a27d8f82be228cde202d097dd13f
Pavel Mishakov <pashamishakov@gmail.com>
2021-05-07 16:09:09 +0600

    fix

--- app/config.js
+++ app/config.js
@@ -4,11 +4,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
     mongoUrl: {
-        // url: "mongodb://localhost/",
-        url: "mongodb+srv://QWE123:QWE123@cluster0.rrd3k.mongodb.net/",
+        url: "mongodb://localhost/",
+        // url: "mongodb+srv://QWE123:QWE123@cluster0.rrd3k.mongodb.net/",
         db: "as_telega2"
     },
-    // telegramToken: "1514560951:AAFtVl-OZIRVrZNGmcsU6zgsmK8oKd1Qcfs", // main
-    telegramToken: "1764913546:AAFBdFo9td0BNJLb9Ir5eM7KTEKKbwo9WVQ",
+    telegramToken: "1514560951:AAFtVl-OZIRVrZNGmcsU6zgsmK8oKd1Qcfs",
+    // telegramToken: "1764913546:AAFBdFo9td0BNJLb9Ir5eM7KTEKKbwo9WVQ", // testing
     telegramPort: 8001
 };
