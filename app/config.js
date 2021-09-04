0b78b9a0daf90d75763bbb95bd8149e8d84721b2
Pavel Mishakov <pashamishakov@gmail.com>
2021-09-04 19:04:26 +0600

    rehide tokens

--- app/config.js
+++ app/config.js
@@ -1,15 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-var tokens_1 = require("../tokens");
 exports.config = {
     mongoUrl: {
         url: "mongodb://localhost/",
         // url: "mongodb+srv://QWE123:QWE123@cluster0.rrd3k.mongodb.net/",
         db: "as_telega_3"
     },
-    // telegramToken: tokens.main, // main
-    telegramToken: tokens_1.tokens.test,
+    // telegramToken: "", // main  LOOK FOR TOKENS IN TOKENS.TXT FILE IN ROOT FILE
+    telegramToken: "",
     telegramPort: 8001,
     accountant: "Динаре @attractor_almaty22" // лучше писать в родительском падеже, так как в сообщениях группам там имено Кому Чему обращаться по поводу оплаты и прочего бухгалтерского
 };
